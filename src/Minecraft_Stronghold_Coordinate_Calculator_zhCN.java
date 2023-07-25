4695cf697c32ffc13fb968c4a9ca80d225dab2d5
zhongguorenn <117806435+zhongguorenn@users.noreply.github.com>
2023-07-25 21:14:10 +0800

    Update Minecraft_Stronghold_Coordinate_Calculator_zhCN.java

--- src/Minecraft_Stronghold_Coordinate_Calculator_zhCN.java
+++ src/Minecraft_Stronghold_Coordinate_Calculator_zhCN.java
@@ -5,6 +5,8 @@
  * 改了好几版这版应该能成
  * 注：本程序中的y为函数中的y而不是游戏中的y
  * 项目地址https://github.com/zhongguorenn/Minecraft_Stronghold_Coordinate_Calculator
+ * @author zhongguorenn
+ * @version 1.0.1
  */
 
 import java.util.Scanner;
@@ -38,6 +40,10 @@ public class Minecraft_Stronghold_Coordinate_Calculator_zhCN {
         double Ex2= s.nextDouble();
         System.out.println("第二个落点z坐标");
         double Ey2= s.nextDouble();
+        System.out.println("是否启用加速算法");
+        String YN = s.nextLine();
+        boolean enable;
+        enable= YN.equals("y");
         double k1 = (Ey1-Sy1)/(Ex1-Sx1);
         double k2 = (Ey2-Sy2)/(Ex2-Sx2);
         double b1 = (Sy1-Sx1*k1);
@@ -56,7 +62,7 @@ public class Minecraft_Stronghold_Coordinate_Calculator_zhCN {
         //-----------------------分割线-------------------------------
         // 反函数算法加速模块
         //使无论何时较小的x变化能引起较大的y值变化
-        if(((k1>0&k1<1)&(k2>0&k2<1))|((k1<0&k1>-1)&(k2<0&k2>-1))|((k1>0&k1<1)&(k2<0&k2>-1))|(k1<0&k1>-1)&(k2>0&k2<1)){
+        if((((k1>0&k1<1)&(k2>0&k2<1))|((k1<0&k1>-1)&(k2<0&k2>-1))|((k1>0&k1<1)&(k2<0&k2>-1))|(k1<0&k1>-1)&(k2>0&k2<1))&enable){
             wheelKValue=true;
             k1=(1/k1);
             k2=(1/k2);
@@ -66,7 +72,7 @@ public class Minecraft_Stronghold_Coordinate_Calculator_zhCN {
         //-----------------------分割线-------------------------------
         //函数y轴对称算法加速模块
         // 将位于负半轴的交点对称到正半轴,以减少遍历时间
-        if(Math.abs((k1+b1-k2-b2))>Math.abs((-k1+b1+k2-b2))){
+        if((Math.abs((k1+b1-k2-b2))>Math.abs((-k1+b1+k2-b2)))&enable){
             leftToRight=true;
             k1=-k1;
             k2=-k2;
@@ -95,8 +101,9 @@ public class Minecraft_Stronghold_Coordinate_Calculator_zhCN {
                     x = -x;
                     if (wheelKValue) {
                         //反函数算法还原
-                        x = (1 / x);
-                        y = (1 / y );
+                        double Tmp = x;
+                        x = y;
+                        y = Tmp;
                     }
                 }
                 x = (int) x;
